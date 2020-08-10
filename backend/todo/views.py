7205288743286ef0deb152d5ddfe5bd4c80d7513
Shavolski-dev <stevewachiradev@gmail.com>
2020-08-10 13:57:21 -0400

    created a todo class view to handle CRUD operations

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import TodoSerializers
+from .models import Todo
 # Create your views here.
+
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
