801f3c30fe6c5382a3382f4d86234d944ef6c639
Shavolski-dev <stevewachiradev@gmail.com>
2020-08-13 14:21:28 -0400

    added default values to populate the list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { Component } from 'react';
+import './App.scss';
+//dummy code just to see how these items will appear on the site
+const todoItems = [
+  {
+    id: 1,
+    title: "Go to Market",
+    description: "Buy ingredients to prepare dinner",
+    completed: true
+  },
+  {
+    id: 2,
+    title: "Study",
+    description: "Read Algebra and History textbook for upcoming test",
+    completed: false
+  },
+  {
+    id: 3,
+    title: "Sally's books",
+    description: "Go to library to rent sally's books",
+    completed: true
+  },
+  {
+    id: 4,
+    title: "Article",
+    description: "Write article on how to use django with react",
+    completed: false
+  }
+];
+//this will handle how the api shall appear on the site
